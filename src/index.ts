bf8c8c892129b5656313d40a71d456799faaae78
jaen <jaenfigueroa03042000@gmail.com>
2023-06-18 20:49:09 -0500

    :sparkles: config knex with pg

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import { age } from './datos'
+import { development } from '../knexfile';
+import express from 'express';
+import knex from 'knex';
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log('hola mundo como estas, genial')
-console.log(age)
-console.log(age)
+const app = express();
+
+// Configuración de la conexión a la base de datos
+const db = knex(development);
+
+// Ejemplo de ruta que utiliza Knex para realizar una consulta
+app.get('/contacts', (req, res) => {
+  db.select('*')
+    .from('Contacts')
+    .then(contact => {
+      res.json(contact);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener los usuarios' });
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
