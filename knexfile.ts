d0f21cbcbcd3e47faaaf6d5cdaf1a7e715f3524e
jaen <jaenfigueroa03042000@gmail.com>
2023-06-18 22:43:14 -0500

    :wrench: chore: config eslint

--- knexfile.ts
+++ knexfile.ts
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const development = {
   client: 'pg',
@@ -8,13 +8,13 @@ export const development = {
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER, //tu_usuario
     password: process.env.DB_PASSWORD, //tu_contraseña
-    database: process.env.DB_DATABASE //nombre_de_la_base_de_datos
+    database: process.env.DB_DATABASE, //nombre_de_la_base_de_datos
   },
   migrations: {
-    directory: './db/migrations'
+    directory: './db/migrations',
   },
   seeds: {
-    directory: './db/seeds'
-  }
+    directory: './db/seeds',
+  },
   // Configuraciones para otros entornos (como producción) si es necesario
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import { development } from '../knexfile';
-import express from 'express';
-import knex from 'knex';
+import { development } from '../knexfile'
+import express from 'express'
+import knex from 'knex'
 
-const app = express();
+const app = express()
 
 // Configuración de la conexión a la base de datos
-const db = knex(development);
+const db = knex(development)
 
 // Crear mis rutas
 app.get('/contacts', (req, res) => {
   db.select('*')
     .from('contacts')
     .then(contact => {
-      res.json(contact);
+      res.json(contact)
     })
     .catch(error => {
-      console.error(error);
-      res.status(500).json({ error: 'Error al obtener los usuarios' });
-    });
-});
+      console.error(error)
+      res.status(500).json({ error: 'Error al obtener los usuarios' })
+    })
+})
 
-// Iniciar el servidor
 app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
-});
+  console.log('Servidor escuchando en el puerto 3000')
+})
